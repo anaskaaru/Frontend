e3a0b2839cc417f3c64b3afbe08899fff11ff0b2
anaskaaru <156852224+anaskaaru@users.noreply.github.com>
2024-03-25 19:09:35 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -12,8 +12,8 @@ const renderTask = (task) => {
     const li = document.createElement('li')
     li.setAttribute('class','list-group-item')
     li.setAttribute('data-key',task.getId().toString())
-    //li.innerHTML = task.getText()
-    renderSpan(li,task.getText())
+    li.innerHTML = task.getText()
+    renderSpan(li)
     renderLink(li,task.getId())
     list.append(li);
 }
